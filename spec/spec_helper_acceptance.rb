cedc7d1c2ba83beb2bce563a4056db592a662d6f
Josh Souza <Josh.Souza@pearson.com>
2015-11-11 15:03:11 -0700

    Fix the testbed basics

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -21,7 +21,7 @@ RSpec.configure do |c|
       copy_module_to(host, :source => proj_root, :module_name => name)
 
       on host, puppet('module install puppetlabs-stdlib --version 4.5.1'), { :acceptable_exit_codes => [0] }
-      on host, puppet('module install puppetlabs-java --version 1.3.0'), { :acceptable_exit_codes => [0] }
+      on host, puppet('module install puppetlabs-java --version 1.4.1'), { :acceptable_exit_codes => [0] }
       on host, puppet('module install deric-zookeeper --version 0.3.5'), { :acceptable_exit_codes => [0] }
 
     end
